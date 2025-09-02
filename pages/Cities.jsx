0809aae420f89ad2eb2e55406fcb2098a8707184
Nika Chitanava <chitanava@gmail.com>
2025-09-02 12:59:21 +0400

    feat: add empty list message

--- pages/Cities.jsx
+++ pages/Cities.jsx
@@ -2,6 +2,7 @@ import Search from "../components/Search.jsx";
 import {useState} from "react";
 import CityList from "../components/CityList.jsx";
 import useFetchCities from "../hooks/useFetchCities.js";
+import EmptyListMessage from "../components/EmptyListMessage.jsx";
 
 export default function Cities() {
     const [cities, loaded] = useFetchCities();
@@ -11,9 +12,11 @@ export default function Cities() {
         ? cities.filter(city => city.cityName.toLowerCase().includes(search.toLowerCase()))
         : cities;
 
-    return loaded && (<>
-        <Search search={search} onSearch={setSearch}/>
-        <CityList cities={filteredCities}/>
-    </>)
+    return loaded && (
+        cities.length > 0
+            ? <><Search search={search} onSearch={setSearch}/>
+                <CityList cities={filteredCities}/></>
+            : <EmptyListMessage/>
+    )
 
 }
--- pages/Countries.jsx
+++ pages/Countries.jsx
@@ -1,8 +1,11 @@
 import CountryList from "../components/CountryList.jsx";
 import useFetchCities from "../hooks/useFetchCities.js";
+import EmptyListMessage from "../components/EmptyListMessage.jsx";
 
 export default function Countries() {
     const [cities, loaded] = useFetchCities();
-
-    return loaded && <CountryList cities={cities}/>
+    
+    return loaded && (cities.length > 0
+        ? <CountryList cities={cities}/>
+        : <EmptyListMessage/>)
 }
